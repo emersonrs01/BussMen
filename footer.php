eba1187293c77e4ec03485a3a692a8c92f502f2c
Emerson Da Rocha <60908442+emersonrs01@users.noreply.github.com>
2021-05-18 19:04:08 -0300

    Versao 2.0

---
+++ footer.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Footer</title>
+	<link rel="stylesheet" href="resources/bootstrap/css/bootstrap.min.css">
+	<script src="resources/jquery/jquery-3.6.0.min.js"></script>
+	<script src="resources/bootstrap/js/bootstrap.min.js"></script>
+	<link href="resources/smartmenus/css/sm-core-css.css" rel="stylesheet" type="text/css"/>
+	<link href="resources/smartmenus/css/sm-mint/sm-mint.css" rel="stylesheet" type="text/css"/>
+	<link rel="stylesheet" href="resources/fontawesome/css/all.css">
+	<link rel="stylesheet" href="resources/estilos.css">
+</head>
+<body>
+    <footer style="display: absolute; left: 0px; top: 100%">
+        <nav class="main-nav" role="navigation">
+	        <ul id="main-menu" class="sm sm-mint">
+	    </ul>
+	    </nav>
+<h4>Powered By ADS/UPF 2021</h4>
+<h6>Edgar / Emerson</h6>
+    </footer>
+</body>
+</html>
---
+++ head.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Cabeçalho APP Aulas</title>
+	<link rel="stylesheet" href="resources/bootstrap/css/bootstrap.min.css">
+	<script src="resources/jquery/jquery-3.6.0.min.js"></script>
+	<script src="resources/bootstrap/js/bootstrap.min.js"></script>
+	<link href="resources/smartmenus/css/sm-core-css.css" rel="stylesheet" type="text/css"/>
+	<link href="resources/smartmenus/css/sm-mint/sm-mint.css" rel="stylesheet" type="text/css"/>
+	<link rel="stylesheet" href="resources/fontawesome/css/all.css">
+	<link rel="stylesheet" href="resources/estilos.css">
+</head>
+<body>
+	<h2>Sistema BussMen</h2>
+    <h6>Mensagens Corporativas</h6>
+		<nav class="main-nav" role="navigation">
+	  <ul id="main-menu" class="sm sm-mint">
+	    <li><a href="home.php">Inicio</a></li>
+	    <li><a href="#">Mensagens</a>
+	      <ul>
+	        <li><a href="/mensagens/enviar.php">Enviar</a></li>
+	        <li><a href="/mensagens/enviadas.php">Enviadas</a></li>
+            <li><a href="/mensagens/recebidas.php">Recebidas</a></li></ul>
+            <li><a href="#">Gestão</a>
+	      <ul>
+	        <li><a href="/gestao/grupos.php">Grupos</a></li>
+	        <li><a href="/gestao/usuarios.php">Usuários</a></li></ul>
+            <li><a href="sair.php">Sair</a></li>
+            <li ali><a href="sair.php">Extras</a>
+            <ul>
+	        <li><a href="http://www.upf.br">UPF.BR</a></li>
+	        <li><a href="http://www.duotech.inf.br">DuoTech Tecnologia</a></li>
+            <li><a href="http://www.compassouol.com">Compasso UOL</a></li></ul>
+        </li>
+	</nav>
+		<script type="text/javascript" src="resources/smartmenus/libs/jquery/jquery.js"></script>
+		<script type="text/javascript" src="resources/smartmenus/jquery.smartmenus.js"></script>
+		<script type="text/javascript">
+		$(function() {
+		$('#main-menu').smartmenus({
+		subMenusSubOffsetX: 1,
+		subMenusSubOffsetY: -8
+				});
+			});
+		</script>
+</body>
+</html>
