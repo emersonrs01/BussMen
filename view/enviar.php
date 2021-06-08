31bf6d511738ae602021c2298b4df8dd1cdfc528
Emerson Da Rocha <60908442+emersonrs01@users.noreply.github.com>
2021-06-08 20:05:59 -0300

    Update enviar.php

--- view/enviar.php
+++ view/enviar.php
@@ -10,20 +10,20 @@
             $pesq = new UserController();
     ?> 
 	<br>
-	<h2>Enviar Mensagem</h2><br>
-    <form id="formenviar" name="formenviar" method="post" action="enviar.php">
+	<h2 style="padding-top:3vh;align-items: center; justify-content:center; text-align: center;">Enviar Mensagem</h2><br>
+    <form id="formenviar" name="formenviar" method="post" action="enviar.php" style="padding-top:3vh;align-items: center; justify-content:center; text-align: center;">
   <p>
     <label>Insira seu Recado Aqui:</label></p>
-    <p><textarea name="mensagem" cols="120" rows="7"></textarea></p>  
+    <p><textarea name="mensagem" cols="120" rows="12"></textarea></p>  
 
     <label for="">Deseja Enviar á um Grupo? </label>
     <select name="envgrp" id="envgrp"><?php $pesq->buscaCadastro(1);?></select><br>
     
     <label for="">Deseja Enviar á um Usuário? </label>
     <select name="envusr" id="envusr"><?php $pesq->buscaCadastro(2);?></select><br>
-
+   
     <input type="submit" name="button" id="button" value="Enviar"></input>
-
+    </form>
     <?php
         include 'footer.php'; 
     ?> 
