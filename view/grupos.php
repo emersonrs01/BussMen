aec4e07e8f1656c891c2bef19b2dd797e836845b
Edgar Neto <edgarneto784@gmail.com>
2021-06-08 20:42:19 -0300

    "FrontEnd" Grupos / Usuários

--- view/grupos.php
+++ view/grupos.php
@@ -4,11 +4,38 @@
 <title>BussMen - Alteração de Grupos</title>
 </head>
 <body>
-    <?php
+<?php
         include("../include/SessaoValidate.php");  
+        include_once("../controller/UserController.php");
+            $pesq = new UserController();
     ?> 
 	<br>
-	<h2>Edição de Grupos de Usuários</h2>
+	<h5 style="padding-top:3vh;align-items: center; justify-content:center; text-align: center;">Adição de Grupos</h5>
+
+    <form id="formgrp" name="formgrp" method="post" action="grupos.php" style="padding-top:3vh;align-items: center; justify-content:center; text-align: center;">
+    <label>Apenas Listando os Grupos Atuais:</label>
+    <select name="listagrp" id="listagrp"><?php $pesq->buscaCadastro(1);?></select><br>
+    <div><label for="group">Digite o Novo Grupo:</label>
+    <input type="text" id="group" name="group"></div>
+    <input type="submit" name="button" id="button" value="Criar"></input><br>
+    </form><br>
+
+    <h5 style="padding-top:3vh;align-items: center; justify-content:center; text-align: center;">Renomear Grupos</h5>
+
+    <form id="formgrp" name="formgrp" method="post" action="grupos.php" style="padding-top:3vh;align-items: center; justify-content:center; text-align: center;">
+        <label>Selecione o Grupo a Ser Renomeado</label>
+        <select name="renomgrp" id="renomgrp"><?php $pesq->buscaCadastro(1);?></select><br>
+        <div><label for="group">Digite o Novo Nome:</label>
+        <input type="text" id="groupnvnm" name="groupnvnm"></div>
+        <input type="submit" name="button" id="button" value="Renomear"></input><br></form><br>
+
+    <h5 style="padding-top:3vh;align-items: center; justify-content:center; text-align: center;">Alteração de Usuários</h5>
+    <form id="formalterausrgrp" name="formalterausrgrp" method="post" action="grupos.php" style="padding-top:3vh;align-items: center; justify-content:center; text-align: center;">
+    <label>Deseja Alterar Qual Usuário? </label>
+    <select name="altgrpusr" id="altgrpusr"><?php $pesq->buscaCadastro(2);?></select><br>
+    <label>Para Qual Grupo? </label>
+    <select name="altusrgrp" id="altusrgrp"><?php $pesq->buscaCadastro(1);?></select><br>
+    <input type="submit" name="button" id="button" value="Alterar"></input><br></form><br>
     <?php
         include 'footer.php'; 
     ?> 
--- view/usuarios.php
+++ view/usuarios.php
@@ -6,9 +6,32 @@
 <body>
     <?php
         include("../include/SessaoValidate.php");  
+        include_once("../controller/UserController.php");
+            $pesq = new UserController();
     ?> 
 	<br>
-	<h2>Edição de Usuários</h2>
+	<h5 style="padding-top:3vh;align-items: center; justify-content:center; text-align: center;">Adição de Usuários</h5>
+
+    <form id="formuser" name="formuser" method="post" action="usuarios.php" style="padding-top:3vh;align-items: center; justify-content:center; text-align: center;">
+    <div><label for="username">Digite o Novo Usuário:</label>
+    <input type="text" id="username" name="username"></div>
+
+    <div><label for="pass">Digite a Senha (8 Caracteres):</label>
+    <input type="password" id="pass" name="password" minlength="8" required></div>
+
+    <label>Este Usuário Pertencerá a Qual Grupo?</label>
+    <select name="envusr" id="envusr"><?php $pesq->buscaCadastro(1);?></select><br>
+    <input type="submit" name="button" id="button" value="Criar"></input></form><br>
+
+    <h5 style="padding-top:3vh;align-items: center; justify-content:center; text-align: center;">Alteração de Senha</h5>
+    <form id="formusergrp" name="formusergrp" method="post" action="usuarios.php" style="padding-top:3vh;align-items: center; justify-content:center; text-align: center;">
+    <label>Deseja Atualizar a Senha de Qual Usuário? </label>
+    <select name="envusr" id="envusr"><?php $pesq->buscaCadastro(2);?></select><br>
+    
+    <div><label for="passalt">Digite a Nova Senha (8 Caracteres):</label>
+    <input type="password" id="passalt" name="password" minlength="8" required></div>
+    <input type="submit" name="button" id="button" value="Alterar"></input></form>
+
     <?php
         include 'footer.php'; 
     ?> 
