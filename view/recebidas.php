f1255f361653217cf159260cc50dd4c4e9b66294
Emerson Da Rocha <60908442+emersonrs01@users.noreply.github.com>
2021-06-14 15:51:25 -0300

    Pagina Recebidas

--- view/recebidas.php
+++ view/recebidas.php
@@ -1,14 +1,28 @@
 <!DOCTYPE html>
+<?php
+        include("../include/SessaoValidate.php");  
+    ?> 
 <html>
 <head>
 <title>Mensagens Recebidas - BussMen</title>
+<link rel="stylesheet" href="../resources/estilosmr.css">
 </head>
 <body>
-    <?php
-        include("../include/SessaoValidate.php");  
-    ?> 
-	<br>
-	<h2>Mensagens Recebidas</h2>
+    <div id="container">
+        <h2>Mensagens Recebidas</h2>
+        <div id="geral">
+            <div id="mrp">
+                <h4 id="mp">Mensagem Pessoa</h4>
+                    <div id="bmp">
+                    </div>
+            </div>
+            <div id="mrg">
+                <h4 id="mg">Mensagem Grupo</h4>
+                    <div id="bmg">
+                    </div>
+            </div>
+        </div>
+    </div>
     <?php
         include 'footer.php'; 
     ?> 
