6f371c49e06c9ff42276a9400d7aad996109853f
Emerson Da Rocha <60908442+emersonrs01@users.noreply.github.com>
2021-06-28 20:16:41 -0300

    Add files via upload

---
+++ view/recebidas.php
@@ -0,0 +1,39 @@
+<?php
+    include("../include/SessaoValidate.php");  
+    include_once("../controller/UserController.php");
+    include_once("../model/User.php");
+    require_once("../model/Mensagem.php");
+    include_once("../model/UserDAO.php");
+    $pesq = new UserController();
+    $obj = new UserController();
+    $obj->inserirMensagemG();
+
+    include 'footer.php'; 
+    ?> 
+<!DOCTYPE html>
+<html>
+<head>
+<title>Mensagens Recebidas - BussMen</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="../resources/estilosmr.css">
+</head>
+<body>
+    <div id="container">
+        <h2>Mensagens Recebidas</h2>
+        <div id="geral">
+            <div id="mrp">
+                <h4 id="mp">Mensagem Pessoa</h4>
+                    <div id="bmp">
+                    <?php $pesq->buscaMensagemG(1);?>
+                    </div>
+            </div>
+            <div id="mrg">
+                <h4 id="mg">Mensagem Grupo</h4>
+                    <div id="bmg">
+                    <?php $pesq->buscaMensagemG(2);?>
+                    </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
